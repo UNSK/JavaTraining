a887db60a29387ac8c2cecaa1524636cc0fe5310
UNSK <hoge@fuga.com>
2013-11-13 15:03:06 +0900

    add 13.11.13

--- ch01/src/ex01_01/HelloWorld.java
+++ ch01/src/ex01_01/HelloWorld.java
@@ -3,7 +3,6 @@ package ex01_01;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello, world");
-
 	}
 
 }
--- ch01/src/ex01_06/Fibonacci.java
+++ ch01/src/ex01_06/Fibonacci.java
@@ -1,13 +1,15 @@
-package ex01_03;
+package ex01_06;
 
 public class Fibonacci {
+	static final int MAX = 50;
+	static final String TITLE = "値が50未満のフィボナッチ数列";
 /** 値が50未満のフィボナッチ数列を表示する. */
 	public static void main(String[] args) {
 		int lo = 1;
 		int hi = 1;
-		System.out.println("値が50未満のフィボナッチ数列"); //タイトル表示
+		System.out.println(TITLE); //タイトル表示
 		System.out.println(lo);
-		while (hi < 50) {
+		while (hi < MAX) {
 			System.out.println(hi);
 			hi = lo + hi;	// 新しいhi
 			lo = hi - lo;	/* 新しいloは、（合計 - 古いlo）
