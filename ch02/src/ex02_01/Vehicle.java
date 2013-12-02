a06dbfbcb481568c24a61fe8e25caa62af21d9eb
UNSK <werdna.b10@gmail.com>
2013-12-02 13:12:43 +0900

    add 13/12/02

---
+++ ch02/src/ex02_01/Vehicle.java
@@ -0,0 +1,10 @@
+package ex02_01;
+
+/**
+ * 現在の速度、方向、所有者を持つ簡単なクラス
+ */
+public class Vehicle {
+	public double speed;
+	public double direction;
+	public String owner;
+}
---
+++ ch02/src/ex02_02/LinkedList.java
@@ -0,0 +1,9 @@
+package ex02_02;
+
+/**
+ * Object型のフィールドと、次のLinkedListへの参照を持つクラス
+ */
+public class LinkedList {
+	public Object obj;
+	public LinkedList next; 
+}
