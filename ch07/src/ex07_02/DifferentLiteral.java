72259866f957209b939b71bcc0c3aaf23bf105b5
UNSK <werdna.b10@gmail.com>
2014-01-29 11:59:24 +0900

    add 14/01/29

---
+++ ch07/src/ex07_02/DifferentLiteral.java
@@ -0,0 +1,39 @@
+package ex07_02;
+
+/**
+ * 7.2 Assign different literal to each primitive numeric types
+ */
+public class DifferentLiteral {
+	//コメントアウトしているものは代入できない
+	//float	
+//	byte   b1 = 3.5f;
+//	short  s1 = 3.5f;
+//	int    i1 = 3.5f;
+//	long   l1 = 3.5f;
+	float  f1 = 3.5f;
+	double d1 = 3.5f;
+	
+	//octal number
+//	byte   b2 = 0777;
+	short  s2 = 0777;
+	int    i2 = 0777;
+	long   l2 = 0777;
+	float  f2 = 0777;
+	double d2 = 0777;
+
+	//long
+//	byte   b3 = 1234L;
+//	short  s3 = 1234L;
+//	int    i3 = 1234L;
+	long   l3 = 1234L;
+	float  f3 = 1234L;
+	double d3 = 1234L;
+
+	//double
+//	byte   b4 = 9.9e9;
+//	short  s4 = 9.9e9;
+//	int    i4 = 9.9e9;
+//	long   l4 = 9.9e9;
+//	float  f4 = 9.9e9;
+	double d4 = 9.9e9;
+}
