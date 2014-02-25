e3d93364d023e497ca2457f2cd06927ada59a05f
UNSK <werdna.b10@gmail.com>
2014-02-25 14:47:47 +0900

    change formatter tab to space

--- GUI/src/ex1_3/DCMenu.java
+++ GUI/src/ex1_3/DCMenu.java
@@ -6,7 +6,6 @@ import java.awt.GraphicsEnvironment;
 import java.awt.Menu;
 import java.awt.MenuItem;
 import java.awt.PopupMenu;
-import java.awt.Window;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.HashMap;
@@ -14,156 +13,156 @@ import java.util.Locale;
 import java.util.Map;
 
 /**
- * MenuBar 
+ * MenuBar
  */
 public class DCMenu extends PopupMenu {
-	/** serial version */
-	private static final long serialVersionUID = 1L;
-	
-	/** minimal font size */
-	private static final int MIN_SIZE = 20;
-	/** max font size */
-	private static final int MAX_SIZE = 80;
-	/** available color list */
-	private static final Color[] COLORS = {
-		Color.BLACK, Color.BLUE, Color.CYAN, Color.DARK_GRAY, Color.GRAY,
-		Color.GREEN, Color.LIGHT_GRAY, Color.MAGENTA, Color.ORANGE, 
-		Color.PINK, Color.RED, Color.WHITE, Color.YELLOW};
-	
-	/**
-	 * constructor 
-	 */
-	public DCMenu(final DigitalClock parent) {
-		super();
-		
-		/* make color map. (K, V) = (color name, awt.Color) */
-		Map<String, Color> colorMap = new HashMap<>();
-		for (Color colors : COLORS) {
-			colorMap.put(getColorName(colors), colors);
-		}
-		
-		Menu mProperties = new Menu("Properties");
-		Menu mFontName = new Menu("Font name");
-		Menu mFontSize = new Menu("Font Size");
-		Menu mFontColor = new Menu("Font Color");
-		Menu mBGColor = new Menu("BGColor");
-		MenuItem mExit = new MenuItem("Exit");
-		
-		/* add font name MenuItems */
-		GraphicsEnvironment ge =
-				GraphicsEnvironment.getLocalGraphicsEnvironment();
-		String[] fonts = ge.getAvailableFontFamilyNames(Locale.US);
-		for (String name : fonts) {
-			final MenuItem mi = new MenuItem(name);
-			mFontName.add(mi);
-			mi.addActionListener(new ActionListener() {
-				@Override
-				public void actionPerformed(ActionEvent e) {
-					Font oldFont = parent.getClockFont();
-					Font newFont = new Font(mi.getLabel(), 
-											oldFont.getStyle(), 
-											oldFont.getSize());
-					parent.setClockFont(newFont);
-				}
-			});
-		}
-		
-		/* add font size MenuItems */
-		for (int i = MIN_SIZE; i < MAX_SIZE; i += 2) {
-			final MenuItem mi = new MenuItem(Integer.toString(i));
-			mFontSize.add(mi);
-			mi.addActionListener(new ActionListener() {
-				@Override
-				public void actionPerformed(ActionEvent e) {
-					Font oldFont = parent.getClockFont();
-					Font newFont = new Font(oldFont.getFontName(), 
-											oldFont.getStyle(), 
-											Integer.parseInt(mi.getLabel()));
-					parent.setClockFont(newFont);
-				}
-			});
-		}
-		
-		/* add font color and bg color MenuItems */
-		for (final Map.Entry<String, Color> entry : colorMap.entrySet()) {
-			final MenuItem fontColoritem = new MenuItem(entry.getKey());
-			final MenuItem bgColoritem = new MenuItem(entry.getKey());
-			mFontColor.add(fontColoritem);
-			mBGColor.add(bgColoritem);
+    /** serial version */
+    private static final long serialVersionUID = 1L;
 
-			fontColoritem.addActionListener(new ActionListener() {
-				@Override
-				public void actionPerformed(ActionEvent e) {
-					parent.setClockColor(entry.getValue());
-				}
-			});
-			
-			bgColoritem.addActionListener(new ActionListener() {
-				@Override
-				public void actionPerformed(ActionEvent e) {
-					parent.setBgColor(entry.getValue());
-				}
-			});
-		}
-		
-		mProperties.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent arg0) {
-				
-				//PropertiesDialog dialog = new PropertiesDialog(frame);
-				//dialog.setVisible(true);
-			}
-		});
-		
-		mExit.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				System.exit(0);
-			}
-		});
-		
-		this.add(mProperties);
-		mProperties.add(mFontName);
-		mProperties.add(mFontColor);
-		mProperties.add(mFontSize);
-		mProperties.add(mBGColor);
-		this.add(mExit);
-	}
-	
-	/**
-	 * convert Color to the color name
-	 * @param color to be convert String
-	 * @return String for color name
-	 */
-	private String getColorName(Color color) {
-		if (color == Color.BLACK) {
-			return "Black";
-		} else if (color == Color.BLUE) {
-			return "Blue";
-		} else if (color == Color.CYAN) {
-			return "Cyan";
-		} else if (color == Color.DARK_GRAY) {
-			return "Dark Gray";
-		} else if (color == Color.GRAY) {
-			return "Gray";
-		} else if (color == Color.GREEN) {
-			return "Green";
-		} else if (color == Color.LIGHT_GRAY) {
-			return "Light Gray";
-		} else if (color == Color.MAGENTA) {
-			return "Magenta";
-		} else if (color == Color.ORANGE) {
-			return "Orange";
-		} else if (color == Color.PINK) {
-			return "Pink";
-		} else if (color == Color.RED) {
-			return "Red";
-		} else if (color == Color.WHITE) {
-			return "White";
-		} else if (color == Color.YELLOW) {
-			return "Yellow";
-		} else {
-			return color.toString();
-		}
-	}
+    /** minimal font size */
+    private static final int MIN_SIZE = 20;
+    /** max font size */
+    private static final int MAX_SIZE = 80;
+    /** available color list */
+    private static final Color[] COLORS = { Color.BLACK, Color.BLUE,
+            Color.CYAN, Color.DARK_GRAY, Color.GRAY, Color.GREEN,
+            Color.LIGHT_GRAY, Color.MAGENTA, Color.ORANGE, Color.PINK,
+            Color.RED, Color.WHITE, Color.YELLOW };
+
+    /**
+     * constructor
+     */
+    public DCMenu(final DigitalClock parent) {
+        super();
+
+        /* make color map. (K, V) = (color name, awt.Color) */
+        Map<String, Color> colorMap = new HashMap<>();
+        for (Color colors : COLORS) {
+            colorMap.put(getColorName(colors), colors);
+        }
+
+        Menu mProperties = new Menu("Properties");
+        Menu mFontName = new Menu("Font name");
+        Menu mFontSize = new Menu("Font Size");
+        Menu mFontColor = new Menu("Font Color");
+        Menu mBGColor = new Menu("BGColor");
+        MenuItem mExit = new MenuItem("Exit");
+
+        /* add font name MenuItems */
+        GraphicsEnvironment ge = GraphicsEnvironment
+                .getLocalGraphicsEnvironment();
+        String[] fonts = ge.getAvailableFontFamilyNames(Locale.US);
+        for (String name : fonts) {
+            final MenuItem mi = new MenuItem(name);
+            mFontName.add(mi);
+            mi.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    Font oldFont = parent.getClockFont();
+                    Font newFont = new Font(mi.getLabel(), oldFont.getStyle(),
+                            oldFont.getSize());
+                    parent.setClockFont(newFont);
+                }
+            });
+        }
+
+        /* add font size MenuItems */
+        for (int i = MIN_SIZE; i < MAX_SIZE; i += 2) {
+            final MenuItem mi = new MenuItem(Integer.toString(i));
+            mFontSize.add(mi);
+            mi.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    Font oldFont = parent.getClockFont();
+                    Font newFont = new Font(oldFont.getFontName(), oldFont
+                            .getStyle(), Integer.parseInt(mi.getLabel()));
+                    parent.setClockFont(newFont);
+                }
+            });
+        }
+
+        /* add font color and bg color MenuItems */
+        for (final Map.Entry<String, Color> entry : colorMap.entrySet()) {
+            final MenuItem fontColoritem = new MenuItem(entry.getKey());
+            final MenuItem bgColoritem = new MenuItem(entry.getKey());
+            mFontColor.add(fontColoritem);
+            mBGColor.add(bgColoritem);
+
+            fontColoritem.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    parent.setClockColor(entry.getValue());
+                }
+            });
+
+            bgColoritem.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    parent.setBgColor(entry.getValue());
+                }
+            });
+        }
+
+        mProperties.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+
+                // PropertiesDialog dialog = new PropertiesDialog(frame);
+                // dialog.setVisible(true);
+            }
+        });
+
+        mExit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+
+        this.add(mProperties);
+        mProperties.add(mFontName);
+        mProperties.add(mFontColor);
+        mProperties.add(mFontSize);
+        mProperties.add(mBGColor);
+        this.add(mExit);
+    }
+
+    /**
+     * convert Color to the color name
+     * 
+     * @param color
+     *            to be convert String
+     * @return String for color name
+     */
+    private String getColorName(Color color) {
+        if (color == Color.BLACK) {
+            return "Black";
+        } else if (color == Color.BLUE) {
+            return "Blue";
+        } else if (color == Color.CYAN) {
+            return "Cyan";
+        } else if (color == Color.DARK_GRAY) {
+            return "Dark Gray";
+        } else if (color == Color.GRAY) {
+            return "Gray";
+        } else if (color == Color.GREEN) {
+            return "Green";
+        } else if (color == Color.LIGHT_GRAY) {
+            return "Light Gray";
+        } else if (color == Color.MAGENTA) {
+            return "Magenta";
+        } else if (color == Color.ORANGE) {
+            return "Orange";
+        } else if (color == Color.PINK) {
+            return "Pink";
+        } else if (color == Color.RED) {
+            return "Red";
+        } else if (color == Color.WHITE) {
+            return "White";
+        } else if (color == Color.YELLOW) {
+            return "Yellow";
+        } else {
+            return color.toString();
+        }
+    }
 }
--- GUI/src/ex1_3/DigitalClock.java
+++ GUI/src/ex1_3/DigitalClock.java
@@ -38,228 +38,228 @@ import java.util.Locale;
  * フォントサイズに応じてフレームの大きさを変更
  */
 public class DigitalClock extends Window implements Runnable {
-	/** serial version */
-	private static final long serialVersionUID = 1L;
-	/** 時計の表示フォーマット */
-	private static final String CLOCK_PATTERN = "yyyy-MM-dd E HH:mm ss";
-	/** タイマーのセット時間（秒） */
-	private static int timerRemain = 0;
-	/** is timer running */
-	private static boolean isTimerRun = false;
-	/** Thread */
-	private Thread thread;
+    /** serial version */
+    private static final long serialVersionUID = 1L;
+    /** 時計の表示フォーマット */
+    private static final String CLOCK_PATTERN = "yyyy-MM-dd E HH:mm ss";
+    /** タイマーのセット時間（秒） */
+    private static int timerRemain = 0;
+    /** is timer running */
+    private static boolean isTimerRun = false;
+    /** Thread */
+    private Thread thread;
 
-	/** Font Settings */
-	private Font clockFont;
-	/** Clock string color */
-	private Color clockColor;
-	/** frame background color */
-	private Color bgColor;
+    /** Font Settings */
+    private Font clockFont;
+    /** Clock string color */
+    private Color clockColor;
+    /** frame background color */
+    private Color bgColor;
 
-	/** buffer strategy */
-	private BufferStrategy buf;
-	/** Location of mouse cursor when mouse downed */
-	private Point cursorLocation;
+    /** buffer strategy */
+    private BufferStrategy buf;
+    /** Location of mouse cursor when mouse downed */
+    private Point cursorLocation;
 
-	/**
-	 * set title, window size. add EventListener start thread
-	 */
-	public DigitalClock() {
-		super(new Frame());
+    /**
+     * set title, window size. add EventListener start thread
+     */
+    public DigitalClock() {
+        super(new Frame());
 
-		setSize(500, 120);
-		setLayout(new FlowLayout(200));
+        setSize(500, 120);
+        setLayout(new FlowLayout(200));
 
-		/* add set time field */
-		//FIXME does not run TextField on Window?
-		TextField timerText = new TextField(8);
-		add(timerText);
-		timerText.addTextListener(new TextListener() {
+        /* add set time field */
+        //FIXME does not run TextField on awt.Window?
+        TextField timerText = new TextField(8);
+        add(timerText);
+        timerText.addTextListener(new TextListener() {
 
-			@Override
-			public void textValueChanged(TextEvent textEvent) {
-				TextField t = (TextField) textEvent.getSource();
-				try {
-					if (!isTimerRun) {
-						timerRemain = Integer.parseInt(t.getText());
-						repaint();
-					} else {
-						t.setText("");
-					}
-				} catch (NumberFormatException e) {
-					t.setText(""); // field clear
-				}
-			}
-		});
+            @Override
+            public void textValueChanged(TextEvent textEvent) {
+                TextField t = (TextField) textEvent.getSource();
+                try {
+                    if (!isTimerRun) {
+                        timerRemain = Integer.parseInt(t.getText());
+                        repaint();
+                    } else {
+                        t.setText("");
+                    }
+                } catch (NumberFormatException e) {
+                    t.setText(""); // field clear
+                }
+            }
+        });
 
-		/* add timer start button */
-		Button timerButton = new Button("Timer");
-		add(timerButton);
-		timerButton.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				if (isTimerRun) { // toggle switch
-					isTimerRun = false;
-				} else {
-					isTimerRun = true;
-				}
-			}
-		});
+        /* add timer start button */
+        Button timerButton = new Button("Timer");
+        add(timerButton);
+        timerButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (isTimerRun) { // toggle switch
+                    isTimerRun = false;
+                } else {
+                    isTimerRun = true;
+                }
+            }
+        });
 
-		addWindowListener(new WindowAdapter() {
-			// close window
-			@Override
-			public void windowClosing(WindowEvent e) {
-				System.exit(0);
-			}
-		});
+        addWindowListener(new WindowAdapter() {
+            // close window
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
 
-		final PopupMenu popupMenu = new DCMenu(this);
-		add(popupMenu);
-		addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseReleased(MouseEvent e) {
-				if (e.getButton() == MouseEvent.BUTTON3) {
-					popupMenu.show(DigitalClock.this, e.getX(), e.getY());
-				}
-			}
+        final PopupMenu popupMenu = new DCMenu(this);
+        add(popupMenu);
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                if (e.getButton() == MouseEvent.BUTTON3) {
+                    popupMenu.show(DigitalClock.this, e.getX(), e.getY());
+                }
+            }
 
-			@Override
-			public void mousePressed(MouseEvent e) {
-				cursorLocation = e.getPoint();
-			}
-		});
+            @Override
+            public void mousePressed(MouseEvent e) {
+                cursorLocation = e.getPoint();
+            }
+        });
 
-		//TODO listen left button only
-		addMouseMotionListener(new MouseMotionAdapter() {
-			@Override
-			public void mouseDragged(MouseEvent e) {
-				Point draggedPoint = e.getPoint();
-				int xDistance = cursorLocation.x - draggedPoint.x;
-				int yDistance = cursorLocation.y - draggedPoint.y;
-				DigitalClock.this.setLocation(getLocation().x - xDistance,
-						getLocation().y - yDistance);
-			}
+        // TODO listen left button only
+        addMouseMotionListener(new MouseMotionAdapter() {
+            @Override
+            public void mouseDragged(MouseEvent e) {
+                Point draggedPoint = e.getPoint();
+                int xDistance = cursorLocation.x - draggedPoint.x;
+                int yDistance = cursorLocation.y - draggedPoint.y;
+                DigitalClock.this.setLocation(getLocation().x - xDistance,
+                        getLocation().y - yDistance);
+            }
 
-		});
+        });
 
-		clockFont = new Font(Font.MONOSPACED, Font.PLAIN, 20);
-		clockColor = Color.BLACK;
-		bgColor = Color.WHITE;
+        clockFont = new Font(Font.MONOSPACED, Font.PLAIN, 20);
+        clockColor = Color.BLACK;
+        bgColor = Color.WHITE;
 
-		setVisible(true);
-		createBufferStrategy(2);
-		buf = getBufferStrategy();
-		this.startThread();
-	}
+        setVisible(true);
+        createBufferStrategy(2);
+        buf = getBufferStrategy();
+        this.startThread();
+    }
 
-	/** Main method */
-	public static void main(String[] args) {
-		new DigitalClock();
-	}
+    /** Main method */
+    public static void main(String[] args) {
+        new DigitalClock();
+    }
 
-	@Override
-	public void paint(Graphics g) {
-		//FIXME this throws nullpo
-		Graphics bufGraphics = buf.getDrawGraphics();
-		if (!buf.contentsLost()) {
-			setBackground(bgColor);
-			bufGraphics.clearRect(0, 0, getSize().width, getSize().height);
-			bufGraphics.setFont(clockFont);
-			bufGraphics.setColor(clockColor);
+    @Override
+    public void paint(Graphics g) {
+        // FIXME this throws nullpo
+        Graphics bufGraphics = buf.getDrawGraphics();
+        if (!buf.contentsLost()) {
+            setBackground(bgColor);
+            bufGraphics.clearRect(0, 0, getSize().width, getSize().height);
+            bufGraphics.setFont(clockFont);
+            bufGraphics.setColor(clockColor);
 
-			FontMetrics fm = bufGraphics.getFontMetrics();
-			Rectangle textRec = fm.getStringBounds(fetchCurrentTime(),
-					bufGraphics).getBounds();
-			int newWidth = 200 + textRec.width;
-			int newHeight = 120 + textRec.height;
-			setSize(newWidth, newHeight);
-			bufGraphics.drawString(fetchCurrentTime(), 100, 120
-					- textRec.height / 2 + fm.getMaxAscent());
-			if (isTimerRun) {
-				bufGraphics.drawString("Timer " + ((Integer) timerRemain)
-						+ " sec", 200, 90);
-			} else {
-				bufGraphics.drawString("      " + ((Integer) timerRemain)
-						+ " sec", 200, 90);
-			}
-			buf.show();
-			bufGraphics.dispose();
-		}
-	}
+            FontMetrics fm = bufGraphics.getFontMetrics();
+            Rectangle textRec = fm.getStringBounds(fetchCurrentTime(),
+                    bufGraphics).getBounds();
+            int newWidth = 200 + textRec.width;
+            int newHeight = 120 + textRec.height;
+            setSize(newWidth, newHeight);
+            bufGraphics.drawString(fetchCurrentTime(), 100, 120
+                    - textRec.height / 2 + fm.getMaxAscent());
+            if (isTimerRun) {
+                bufGraphics.drawString("Timer " + ((Integer) timerRemain)
+                        + " sec", 200, 90);
+            } else {
+                bufGraphics.drawString("      " + ((Integer) timerRemain)
+                        + " sec", 200, 90);
+            }
+            buf.show();
+            bufGraphics.dispose();
+        }
+    }
 
-	/**
-	 * fetch current time to string
-	 * 
-	 * @return フォーマットで指定された形式の現時刻を表す文字列
-	 */
-	public String fetchCurrentTime() {
-		Calendar calendar = Calendar.getInstance();
-		DateFormat df = new SimpleDateFormat(CLOCK_PATTERN, Locale.US);
-		return df.format(calendar.getTime());
-	}
+    /**
+     * fetch current time to string
+     * 
+     * @return フォーマットで指定された形式の現時刻を表す文字列
+     */
+    public String fetchCurrentTime() {
+        Calendar calendar = Calendar.getInstance();
+        DateFormat df = new SimpleDateFormat(CLOCK_PATTERN, Locale.US);
+        return df.format(calendar.getTime());
+    }
 
-	/** start thread */
-	public void startThread() {
-		thread = new Thread(this);
-		thread.start();
-	}
+    /** start thread */
+    public void startThread() {
+        thread = new Thread(this);
+        thread.start();
+    }
 
-	@Override
-	public void run() {
-		while (thread == Thread.currentThread()) {
-			repaint();
-			if (isTimerRun) {
-				if (timerRemain == 0) {
-					isTimerRun = false;
-				} else {
-					timerRemain--;
-				}
-			}
-			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e) {
-				System.out.println("thread failed");
-			}
-		}
-	}
+    @Override
+    public void run() {
+        while (thread == Thread.currentThread()) {
+            repaint();
+            if (isTimerRun) {
+                if (timerRemain == 0) {
+                    isTimerRun = false;
+                } else {
+                    timerRemain--;
+                }
+            }
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                System.out.println("thread failed");
+            }
+        }
+    }
 
-	/**
-	 * @param font
-	 *            the clockFont to set
-	 */
-	public void setClockFont(Font font) {
-		clockFont = font;
-	}
+    /**
+     * @param font
+     *            the clockFont to set
+     */
+    public void setClockFont(Font font) {
+        clockFont = font;
+    }
 
-	/** @return the ClockFont */
-	public Font getClockFont() {
-		return clockFont;
-	}
+    /** @return the ClockFont */
+    public Font getClockFont() {
+        return clockFont;
+    }
 
-	/**
-	 * @param color
-	 *            the clockColor to set
-	 */
-	public void setClockColor(Color color) {
-		clockColor = color;
-	}
+    /**
+     * @param color
+     *            the clockColor to set
+     */
+    public void setClockColor(Color color) {
+        clockColor = color;
+    }
 
-	/** @return the clockColor */
-	public Color getClockColor() {
-		return clockColor;
-	}
+    /** @return the clockColor */
+    public Color getClockColor() {
+        return clockColor;
+    }
 
-	/**
-	 * @param color
-	 *            the bgColor to set
-	 */
-	public void setBgColor(Color color) {
-		bgColor = color;
-	}
+    /**
+     * @param color
+     *            the bgColor to set
+     */
+    public void setBgColor(Color color) {
+        bgColor = color;
+    }
 
-	/** @return the bgColor */
-	public Color getBgColor() {
-		return bgColor;
-	}
+    /** @return the bgColor */
+    public Color getBgColor() {
+        return bgColor;
+    }
 }
