909717f7311cbdbd34e5bbc9e1ace667bbff44f4
UNSK <werdna.b10@gmail.com>
2014-02-25 14:36:35 +0900

    added dragging to move windows

--- GUI/src/ex1_3/DigitalClock.java
+++ GUI/src/ex1_3/DigitalClock.java
@@ -7,7 +7,10 @@ import java.awt.Font;
 import java.awt.FontMetrics;
 import java.awt.Frame;
 import java.awt.Graphics;
+import java.awt.GraphicsConfiguration;
+import java.awt.GraphicsDevice;
 import java.awt.Panel;
+import java.awt.Point;
 import java.awt.PopupMenu;
 import java.awt.Rectangle;
 import java.awt.TextField;
@@ -17,21 +20,22 @@ import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionAdapter;
 import java.awt.event.TextEvent;
 import java.awt.event.TextListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.awt.geom.AffineTransform;
 import java.awt.image.BufferStrategy;
+import java.awt.image.ColorModel;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Locale;
 
 /**
- * 　課題1-2 メニューの追加、プロパティダイアログの追加
- * 		プロパティダイアログ内にフォント指定の追加
- * 		ダブルバッファリングの実装
- * 		フォントサイズに応じてフレームの大きさを変更
+ * 　課題1-2 メニューの追加、プロパティダイアログの追加 プロパティダイアログ内にフォント指定の追加 ダブルバッファリングの実装
+ * フォントサイズに応じてフレームの大きさを変更
  */
 public class DigitalClock extends Window implements Runnable {
 	/** serial version */
@@ -44,34 +48,34 @@ public class DigitalClock extends Window implements Runnable {
 	private static boolean isTimerRun = false;
 	/** Thread */
 	private Thread thread;
-	
-	/** Font Settings*/
-	private Font clockFont; 
+
+	/** Font Settings */
+	private Font clockFont;
 	/** Clock string color */
 	private Color clockColor;
 	/** frame background color */
 	private Color bgColor;
-	
+
 	/** buffer strategy */
 	private BufferStrategy buf;
-	
+	/** Location of mouse cursor when mouse downed */
+	private Point cursorLocation;
+
 	/**
-	 * set title, window size. add EventListener
-	 * start thread
+	 * set title, window size. add EventListener start thread
 	 */
 	public DigitalClock() {
 		super(new Frame());
+
 		setSize(500, 120);
-		//setResizable(false);
 		setLayout(new FlowLayout(200));
-		//setMenuBar(new DCMenu(this).getMenuBar());
-		
-		
+
 		/* add set time field */
+		//FIXME does not run TextField on Window?
 		TextField timerText = new TextField(8);
 		add(timerText);
 		timerText.addTextListener(new TextListener() {
-			
+
 			@Override
 			public void textValueChanged(TextEvent textEvent) {
 				TextField t = (TextField) textEvent.getSource();
@@ -80,14 +84,14 @@ public class DigitalClock extends Window implements Runnable {
 						timerRemain = Integer.parseInt(t.getText());
 						repaint();
 					} else {
-						t.setText("");	
+						t.setText("");
 					}
 				} catch (NumberFormatException e) {
 					t.setText(""); // field clear
 				}
 			}
 		});
-		
+
 		/* add timer start button */
 		Button timerButton = new Button("Timer");
 		add(timerButton);
@@ -101,7 +105,7 @@ public class DigitalClock extends Window implements Runnable {
 				}
 			}
 		});
-			
+
 		addWindowListener(new WindowAdapter() {
 			// close window
 			@Override
@@ -109,8 +113,7 @@ public class DigitalClock extends Window implements Runnable {
 				System.exit(0);
 			}
 		});
-		
-		
+
 		final PopupMenu popupMenu = new DCMenu(this);
 		add(popupMenu);
 		addMouseListener(new MouseAdapter() {
@@ -120,12 +123,30 @@ public class DigitalClock extends Window implements Runnable {
 					popupMenu.show(DigitalClock.this, e.getX(), e.getY());
 				}
 			}
+
+			@Override
+			public void mousePressed(MouseEvent e) {
+				cursorLocation = e.getPoint();
+			}
 		});
-		
+
+		//TODO listen left button only
+		addMouseMotionListener(new MouseMotionAdapter() {
+			@Override
+			public void mouseDragged(MouseEvent e) {
+				Point draggedPoint = e.getPoint();
+				int xDistance = cursorLocation.x - draggedPoint.x;
+				int yDistance = cursorLocation.y - draggedPoint.y;
+				DigitalClock.this.setLocation(getLocation().x - xDistance,
+						getLocation().y - yDistance);
+			}
+
+		});
+
 		clockFont = new Font(Font.MONOSPACED, Font.PLAIN, 20);
 		clockColor = Color.BLACK;
 		bgColor = Color.WHITE;
-		
+
 		setVisible(true);
 		createBufferStrategy(2);
 		buf = getBufferStrategy();
@@ -139,35 +160,37 @@ public class DigitalClock extends Window implements Runnable {
 
 	@Override
 	public void paint(Graphics g) {
+		//FIXME this throws nullpo
 		Graphics bufGraphics = buf.getDrawGraphics();
 		if (!buf.contentsLost()) {
-		setBackground(bgColor);
-		bufGraphics.clearRect(0, 0, getSize().width, getSize().height);
-		bufGraphics.setFont(clockFont);
-		bufGraphics.setColor(clockColor);
-		
-		FontMetrics fm = bufGraphics.getFontMetrics();
-		Rectangle textRec = fm.getStringBounds(
-				fetchCurrentTime(), bufGraphics).getBounds();
-		int newWidth = 200 + textRec.width;
-		int newHeight = 120 + textRec.height;
-		setSize(newWidth, newHeight);
-		bufGraphics.drawString(fetchCurrentTime(), 100,
-				120 - textRec.height/2 + fm.getMaxAscent());
-		if (isTimerRun) {
-			bufGraphics.drawString("Timer " 
-					+ ((Integer) timerRemain) + " sec", 200, 90);
-		} else {
-			bufGraphics.drawString("      " 
-					+ ((Integer) timerRemain) + " sec", 200, 90);
-		}
-		buf.show();
-		bufGraphics.dispose();
+			setBackground(bgColor);
+			bufGraphics.clearRect(0, 0, getSize().width, getSize().height);
+			bufGraphics.setFont(clockFont);
+			bufGraphics.setColor(clockColor);
+
+			FontMetrics fm = bufGraphics.getFontMetrics();
+			Rectangle textRec = fm.getStringBounds(fetchCurrentTime(),
+					bufGraphics).getBounds();
+			int newWidth = 200 + textRec.width;
+			int newHeight = 120 + textRec.height;
+			setSize(newWidth, newHeight);
+			bufGraphics.drawString(fetchCurrentTime(), 100, 120
+					- textRec.height / 2 + fm.getMaxAscent());
+			if (isTimerRun) {
+				bufGraphics.drawString("Timer " + ((Integer) timerRemain)
+						+ " sec", 200, 90);
+			} else {
+				bufGraphics.drawString("      " + ((Integer) timerRemain)
+						+ " sec", 200, 90);
+			}
+			buf.show();
+			bufGraphics.dispose();
 		}
 	}
-	
+
 	/**
 	 * fetch current time to string
+	 * 
 	 * @return フォーマットで指定された形式の現時刻を表す文字列
 	 */
 	public String fetchCurrentTime() {
@@ -181,7 +204,7 @@ public class DigitalClock extends Window implements Runnable {
 		thread = new Thread(this);
 		thread.start();
 	}
-	
+
 	@Override
 	public void run() {
 		while (thread == Thread.currentThread()) {
@@ -200,32 +223,41 @@ public class DigitalClock extends Window implements Runnable {
 			}
 		}
 	}
-	
-	/** @param font the clockFont to set */
+
+	/**
+	 * @param font
+	 *            the clockFont to set
+	 */
 	public void setClockFont(Font font) {
 		clockFont = font;
 	}
-	
+
 	/** @return the ClockFont */
 	public Font getClockFont() {
 		return clockFont;
 	}
-	
-	/** @param color the clockColor to set */
+
+	/**
+	 * @param color
+	 *            the clockColor to set
+	 */
 	public void setClockColor(Color color) {
 		clockColor = color;
 	}
-	
+
 	/** @return the clockColor */
 	public Color getClockColor() {
 		return clockColor;
 	}
-	
-	/** @param color the bgColor to set */
+
+	/**
+	 * @param color
+	 *            the bgColor to set
+	 */
 	public void setBgColor(Color color) {
 		bgColor = color;
 	}
-	
+
 	/** @return the bgColor */
 	public Color getBgColor() {
 		return bgColor;
