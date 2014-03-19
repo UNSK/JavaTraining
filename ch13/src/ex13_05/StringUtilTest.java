f147abb64f9b790f75032aa99ea18117b7d90d60
UNSK <werdna.b10@gmail.com>
2014-03-19 10:18:10 +0900

    tested delimitNumeral

--- ch13/src/ex13_05/StringUtilTest.java
+++ ch13/src/ex13_05/StringUtilTest.java
@@ -2,6 +2,7 @@ package ex13_05;
 
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.CoreMatchers.*;
+import static ex13_05.StringUtil.*;
 
 import java.util.ArrayList;
 
@@ -14,33 +15,33 @@ public class StringUtilTest {
 
     @Test
     public void countCharTest() {
-        assertThat(StringUtil.countChar("abbbc", 'b'), is(3));
-        assertThat(StringUtil.countChar("abc", 'x'), is(0));
-        assertThat(StringUtil.countChar("aaaaa", 'a'), is(5));
+        assertThat(countChar("abbbc", 'b'), is(3));
+        assertThat(countChar("abc", 'x'), is(0));
+        assertThat(countChar("aaaaa", 'a'), is(5));
     }
     
     @Test
     public void countMachedRegionTest() {
-        assertThat(StringUtil.countMachedRegion("abcabcab", "abc"), is(2));
-        assertThat(StringUtil.countMachedRegion("oooooo", "oo"), is(5));
-        assertThat(StringUtil.countMachedRegion("abcabcabc", "xyz"), is(0));
+        assertThat(countMachedRegion("abcabcab", "abc"), is(2));
+        assertThat(countMachedRegion("oooooo", "oo"), is(5));
+        assertThat(countMachedRegion("abcabcabc", "xyz"), is(0));
     }
     
     
     @Test
     public void delimitedStringTest() {
-        String[] case1 = StringUtil.delimitedString("aaa<bbb>ccc<ddd>eeee", '<', '>');
+        String[] case1 = delimitedString("aaa<bbb>ccc<ddd>eeee", '<', '>');
         assertThat(case1.length, is(2));
         assertThat(case1[0], is("<bbb>"));
         assertThat(case1[1], is("<ddd>"));
         
-        String[] case2 = StringUtil.delimitedString("foobar", '#', '%');
+        String[] case2 = delimitedString("foobar", '#', '%');
         assertThat(case2.length, is(0));
         
-        String[] case3 = StringUtil.delimitedString("aaa><aaa", '<', '>');
+        String[] case3 = delimitedString("aaa><aaa", '<', '>');
         assertThat(case3.length, is(0));
         
-        String[] case4 = StringUtil.delimitedString("low<high", '<', '>');
+        String[] case4 = delimitedString("low<high", '<', '>');
         assertThat(case4.length, is(1));
         assertThat(case4[0], is("<high"));
     }
@@ -48,8 +49,17 @@ public class StringUtilTest {
     @Test
     public void processTypeValuePairTest() {
         String input = "Integer 123\nBoolean false\n";
-        ArrayList<Object> out = StringUtil.processTypeValuePair(input);
+        ArrayList<Object> out = processTypeValuePair(input);
         assertThat((Integer) out.get(0), is(123));
-        assertThat((Boolean) out.get(2), is(false));
+        assertThat((Boolean) out.get(1), is(false));
+    }
+    
+    @Test
+    public void delimitNumeralTest() {
+        assertThat(delimitNumeral("123"), is("123"));
+        assertThat(delimitNumeral("1234"), is("1,234"));
+        assertThat(delimitNumeral("12345"), is("12,345"));
+        assertThat(delimitNumeral("123456"), is("123,456"));
+        assertThat(delimitNumeral("1234567"), is("1,234,567"));
     }
 }
