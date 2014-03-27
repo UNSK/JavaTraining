8e6e2a0af8bb1bc38a59a84be459d34fc7636a27
UNSK <werdna.b10@gmail.com>
2014-03-27 10:43:04 +0900

    implemented and tested

--- ch14/src/ex14_09/ThreadGroupObserver.java
+++ ch14/src/ex14_09/ThreadGroupObserver.java
@@ -20,7 +20,9 @@ public class ThreadGroupObserver extends Thread {
     public void run() {
         try {
           for(;;) {
-              printThreadGroup();
+              System.out.println("** print threads begin **");
+              printThreadGroup(threadGroup);
+              System.out.println("** end **");
               Thread.sleep(interval);
           }
         } catch (InterruptedException e) {
@@ -28,7 +30,63 @@ public class ThreadGroupObserver extends Thread {
         }
     }
     
-    private void printThreadGroup() {
-        System.out.println(threadGroup);
+    private void printThreadGroup(ThreadGroup threadGroup) {
+        Thread[] threads = new Thread[threadGroup.activeCount()];
+        ThreadGroup[] groups = new ThreadGroup[threadGroup.activeGroupCount()];
+        
+        System.out.printf("< %s >%n", threadGroup.getName());
+        threadGroup.enumerate(threads, false);
+        for (Thread t : threads) {
+            if (t != null)
+                System.out.println("\t" + t.getName());
+        }
+        threadGroup.enumerate(groups, false);
+        for (ThreadGroup tg : groups) {
+            if(tg != null)
+                printThreadGroup(tg);
+        }
+    }
+    
+    public static void main(String[] args) throws InterruptedException {
+        
+        class Sleep implements Runnable {
+            private long lifetime;
+            Sleep(long lifetime) {
+                this.lifetime = lifetime;
+            }
+            
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(lifetime);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }
+        
+        ThreadGroup root = new ThreadGroup("ROOT");
+        ThreadGroup alpha = new ThreadGroup(root, "Alpha");
+        ThreadGroup bravo = new ThreadGroup(root, "Bravo");
+        ThreadGroup bravo1 = new ThreadGroup(bravo, "Bravo-1");
+        ThreadGroup bravo2 = new ThreadGroup(bravo, "Bravo-2");
+        
+        ThreadGroupObserver observer = new ThreadGroupObserver(root, 2000);
+        observer.start();
+        
+        for (;;) {
+            new Thread(root, new Sleep(3000), "root-a").start();
+            new Thread(root, new Sleep(5000), "root-b").start();
+            new Thread(alpha, new Sleep(2000), "alpha-a").start();
+            new Thread(alpha, new Sleep(4000), "alpha-b").start();
+            new Thread(alpha, new Sleep(1000), "alpha-c").start();
+            new Thread(bravo, new Sleep(9000), "bravo-a").start();
+            new Thread(bravo, new Sleep(2000), "bravo-b").start();
+            new Thread(bravo1, new Sleep(1000), "bravo-1-a").start();
+            new Thread(bravo2, new Sleep(5000), "bravo-2-a").start();
+            new Thread(bravo2, new Sleep(3000), "bravo-2-b").start();
+            new Thread(bravo2, new Sleep(6000), "bravo-2-c").start();
+            Thread.sleep(5500);;
+        }
     }
 }
