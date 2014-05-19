f7a4de36071533e1eeed2ed1b7a6e523310f2685
UNSK <werdna.b10@gmail.com>
2014-05-19 14:42:57 +0900

    add 14/05/19

---
+++ ch20/src/ex20_11/FileNameSuffixFilter.java
@@ -0,0 +1,34 @@
+package ex20_11;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.io.FilenameFilter;
+import java.nio.file.Files;
+
+public class FileNameSuffixFilter implements FilenameFilter {
+
+    private final String suffix;
+    
+    public FileNameSuffixFilter(String suffix) {
+        this.suffix = suffix;
+    }
+    
+    @Override
+    public boolean accept(File dir, String name) {
+        return name.endsWith(suffix);
+    }
+
+    public static void main(String[] args) {
+        if (args.length != 2) 
+            throw new IllegalArgumentException("input 2 arguments");
+        File dir = new File(args[0]);
+        if (!dir.isDirectory()) 
+            throw new IllegalArgumentException("file is not directory.");
+        String[] files = dir.list(new FileNameSuffixFilter(args[1]));
+        System.out.println(files.length + " file(s)");
+        for (String s : files) {
+            System.out.println("  " + s);
+        }
+    }
+
+}
