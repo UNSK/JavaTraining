e34472e790d9285d3d613bf6fd35f5618f6ab985
UNSK <werdna.b10@gmail.com>
2015-01-09 16:13:12 +0900

    initial commit

---
+++ ChatClient/src/main/java/chat/ChatClient.java
@@ -0,0 +1,25 @@
+/**
+ * Copyright (c) 2015 Ricoh Company, ltd.
+ */
+package chat;
+
+import javax.websocket.ClientEndpoint;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+
+/**
+ *
+ */
+@ClientEndpoint
+public class ChatClient {
+    @OnOpen
+    public void onOpen(Session session) {
+        System.out.println("tsunagatta!");
+    }
+
+    @OnMessage
+    public void onMessage(String message) {
+        System.out.println(message);
+    }
+}
---
+++ ChatClient/src/main/java/chat/ChatStarter.java
@@ -0,0 +1,30 @@
+/**
+ * Copyright (c) 2015 Ricoh Company, ltd.
+ */
+package chat;
+
+import java.io.IOException;
+import java.net.URI;
+
+import javax.websocket.ContainerProvider;
+import javax.websocket.DeploymentException;
+import javax.websocket.Session;
+import javax.websocket.WebSocketContainer;
+
+/**
+ *
+ */
+public class ChatStarter {
+
+    /**
+     * @param args
+     * @throws IOException
+     * @throws DeploymentException
+     */
+    public static void main(String[] args) throws DeploymentException, IOException {
+        WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+        URI uri = URI.create("ws://localhost:8080/chat");
+        Session session = container.connectToServer(new ChatClient(), uri);
+    }
+
+}
